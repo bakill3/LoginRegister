694b65404d7043a01e0b7ffd50fce88babfd4e3d
Gabriel Brandão <35882052+bakill3@users.noreply.github.com>
2019-01-26 17:20:12 +0000

    Login Improved
    More protected, less code, and more efficient. Read the comments for explanation.

--- login.php
+++ login.php
@@ -2,44 +2,48 @@
 
 require 'init.php';
 session_start();
-if(!isset($_SESSION['email'])){
+if(isset($_POST['submit'])){
 
-	$email = isset($_POST['email']) ? $_POST['email'] : '';
-	$password = isset($_POST['password']) ? $_POST['password'] : '';
+	$email = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['email'])); //PROTECTED AGAINST SQL INJECTION AND HTML/JS INJECTION
+	$password = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['password'])); //PROTECTED AGAINST SQL INJECTION AND HTML/JS INJECTION
 
-	$email = htmlspecialchars($email);
-	$password = htmlspecialchars($password);
+	//YOU SHOULD also encrypt passwords (I advise PASSWORD_HASH)
+
+	//$email = htmlspecialchars($email);
+	//$password = htmlspecialchars($password);
 
 	$error=false;
-
-	if(isset($_POST['submit'])){
 	
 		if(!empty($email) && !empty($password)){
+			$length_email = strlen($email); //how many characters in
+			$length_password = strlen($password); //how many characters in
+			if ($length_password < 128 && $length_email < 128) { //user can delete the maxlength="128" by inspecting element
+				//check if it matches
+				$sql = " SELECT `email` FROM `users` WHERE `email` = '$email' AND `password` = '$password'; ";
+				$result = mysqli_query($conn, $sql);
+				if(mysqli_num_rows($result)>0){
+					$error = false;
+				}else{
+					$error = true;
+				}
 			
-			//check if it matches
-			$sql = " SELECT `email` FROM `users` WHERE `email` = '$email' AND `password` = '$password'; ";
-			$result = mysqli_query($conn, $sql);
-			if(mysqli_num_rows($result)>0){
-				$error = false;
-			}else{
-				$error = true;
-			}
-		
-			if($error){
-				echo "Error in email or/and password.";
-			}
-			if(!$error){
-				//login
-				$_SESSION['email']=$email;
-				echo "Logged in.";
-				header("Location: welcome.php");
+				if($error){
+					echo "Error in email or/and password.";
+				}
+				if(!$error){
+					//login
+					$_SESSION['email']=$email;
+					echo "Logged in.";
+					header("Location: welcome.php");
+					exit(0); //close header location
+				}
+			} else {
+				echo "Max lenght is 127";
 			}
 
 		}else{
 			echo "Fill in all the details.";
 		}
-		
-	}
 
 }
 ?>
@@ -49,11 +53,11 @@ if(!isset($_SESSION['email'])){
 <br>
 <br>
 
-<form action="login.php" method="post">
+<form method="post">
 
   Email address:
   <br>
-  <input type="text" name="email" maxlength="128">
+  <input type="email" name="email" maxlength="128">
   <br>
   <br>
   
